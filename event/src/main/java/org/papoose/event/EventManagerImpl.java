ad4f27e87130d5144cec08de36219ec22f2369fa
Alan D. Cabrera <adc@toolazydogs.com>
2010-02-18 01:26:46 +0000

    Intermediate sketch

--- event/src/main/java/org/papoose/event/EventManagerImpl.java
+++ event/src/main/java/org/papoose/event/EventManagerImpl.java
@@ -16,9 +16,14 @@
  */
 package org.papoose.event;
 
+import java.util.Collections;
 import java.util.Dictionary;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Executor;
 import java.util.logging.Logger;
 
 import org.osgi.framework.BundleContext;
@@ -40,24 +45,15 @@ public class EventManagerImpl extends ServiceTracker implements EventAdmin
 {
     private final static String CLASS_NAME = EventManagerImpl.class.getName();
     private final static Logger LOGGER = Logger.getLogger(CLASS_NAME);
-    private final Map<String, Listeners> listeners = new HashMap<String, Listeners>();
     private final static Filter DEFAULT_FILTER = new Filter()
     {
-        public boolean match(ServiceReference serviceReference)
-        {
-            return true;
-        }
+        public boolean match(ServiceReference serviceReference) { return true; }
 
-        public boolean match(Dictionary dictionary)
-        {
-            return true;
-        }
+        public boolean match(Dictionary dictionary) { return true; }
 
-        public boolean matchCase(Dictionary dictionary)
-        {
-            return true;
-        }
+        public boolean matchCase(Dictionary dictionary) { return true; }
     };
+    private final Listeners listeners = new Listeners();
 
     public EventManagerImpl(final BundleContext context)
     {
@@ -105,30 +101,91 @@ public class EventManagerImpl extends ServiceTracker implements EventAdmin
         super.removedService(reference, service);    //Todo change body of overridden methods use File | Settings | File Templates.
     }
 
-    public void postEvent(Event event)
+    public void postEvent(final Event event)
     {
         LOGGER.entering(CLASS_NAME, "", event);
-        //Todo change body of implemented methods use File | Settings | File Templates.
+
+        Set<EventListener> set = collectListeners(event);
+
+        for (final EventListener eventListener : set)
+        {
+            eventListener.executor.execute(new Runnable()
+            {
+                public void run()
+                {
+                    eventListener.handleEvent(event);
+                }
+            });
+        }
+
+        LOGGER.exiting(CLASS_NAME, "postEvent");
     }
 
     public void sendEvent(Event event)
     {
-        //Todo change body of implemented methods use File | Settings | File Templates.
+        LOGGER.entering(CLASS_NAME, "sendEvent", event);
+
+        Set<EventListener> set = collectListeners(event);
+
+        for (EventListener eventListener : set)
+        {
+            eventListener.handleEvent(event);
+        }
+
+        LOGGER.exiting(CLASS_NAME, "sendEvent");
+    }
+
+    private Set<EventListener> collectListeners(Event event)
+    {
+        Listeners l = listeners;
+        Set<EventListener> set = new HashSet<EventListener>();
+
+        addListeners(set, l.getHandlers(), event);
+
+        for (String token : event.getTopic().split("/"))
+        {
+            l = l.getListeners().get(token);
+
+            if (l == null) break;
+
+            addListeners(set, l.getHandlers(), event);
+        }
+        return set;
+    }
+
+    private static void addListeners(Set<EventListener> to, Set<EventListener> from, Event event)
+    {
+        for (EventListener eventListener : from)
+        {
+            if (event.matches(eventListener.getFilter())) to.add(eventListener);
+        }
     }
 
     private static class Listeners
     {
-        Map<String, Listeners> listeners;
+        Map<String, Listeners> listeners = new Hashtable<String, Listeners>();
+        Set<EventListener> handlers = Collections.synchronizedSet(new HashSet<EventListener>());
+
+        public Map<String, Listeners> getListeners()
+        {
+            return listeners;
+        }
+
+        public Set<EventListener> getHandlers()
+        {
+            return handlers;
+        }
     }
 
     private class EventListener implements EventHandler
     {
+        private final Executor executor;
         private final ServiceReference reference;
         private final Filter filter;
 
-
-        private EventListener(ServiceReference reference, String filter) throws InvalidSyntaxException
+        private EventListener(Executor executor, ServiceReference reference, String filter) throws InvalidSyntaxException
         {
+            this.executor = executor;
             this.reference = reference;
             this.filter = (filter == null ? DEFAULT_FILTER : context.createFilter(filter));
         }
@@ -140,7 +197,7 @@ public class EventManagerImpl extends ServiceTracker implements EventAdmin
 
         public void handleEvent(Event event)
         {
-            EventHandler handler = (EventHandler)context.getService(reference);
+            EventHandler handler = (EventHandler) context.getService(reference);
             if (handler != null) handler.handleEvent(event);
         }
     }
