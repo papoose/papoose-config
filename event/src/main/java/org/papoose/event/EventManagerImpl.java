562a285daffdf9354874c0843fd003eef075445b
Alan D. Cabrera <adc@toolazydogs.com>
2010-02-19 15:02:37 +0000

    Final sketch of event admin

--- event/src/main/java/org/papoose/event/EventManagerImpl.java
+++ event/src/main/java/org/papoose/event/EventManagerImpl.java
@@ -23,7 +23,13 @@ import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.osgi.framework.BundleContext;
@@ -34,8 +40,9 @@ import org.osgi.service.event.Event;
 import org.osgi.service.event.EventAdmin;
 import org.osgi.service.event.EventConstants;
 import org.osgi.service.event.EventHandler;
+import org.osgi.service.event.TopicPermission;
 import org.osgi.util.tracker.ServiceTracker;
-import org.osgi.util.tracker.ServiceTrackerCustomizer;
+import org.papoose.event.util.SerialExecutor;
 
 
 /**
@@ -54,102 +61,273 @@ public class EventManagerImpl extends ServiceTracker implements EventAdmin
         public boolean matchCase(Dictionary dictionary) { return true; }
     };
     private final Listeners listeners = new Listeners();
+    private final ExecutorService executor;
+    private final ScheduledExecutorService scheduledExecutor;
+    private int timeout = 60;
+    private TimeUnit timeUnit = TimeUnit.SECONDS;
 
-    public EventManagerImpl(final BundleContext context)
+    public EventManagerImpl(BundleContext context, ExecutorService executor, ScheduledExecutorService scheduledExecutor)
     {
-        super(context, EventHandler.class.getName(), new ServiceTrackerCustomizer()
-        {
-            public Object addingService(ServiceReference reference)
-            {
-                LOGGER.entering(CLASS_NAME, "addingService", reference);
+        super(context, EventHandler.class.getName(), null);
 
-                if (reference.getProperty(EventConstants.EVENT_TOPIC) == null)
-                {
-                    LOGGER.finest("Reference does not contain event topic, ignoring");
-                    LOGGER.exiting(CLASS_NAME, "addingService", null);
-                    return null;
-                }
-
-                Object service = context.getService(reference);
-
-                LOGGER.exiting(CLASS_NAME, "addingService", service);
-
-                return service;
-            }
-
-            public void modifiedService(ServiceReference reference, Object service) { }
-
-            public void removedService(ServiceReference reference, Object service) { }
-        });
+        this.executor = executor;
+        this.scheduledExecutor = scheduledExecutor;
     }
 
+    public int getTimeout()
+    {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout)
+    {
+        this.timeout = timeout;
+    }
+
+    public TimeUnit getTimeUnit()
+    {
+        return timeUnit;
+    }
+
+    public void setTimeUnit(TimeUnit timeUnit)
+    {
+        this.timeUnit = timeUnit;
+    }
+
+    /**
+     * This service tracker customizer culls services that do not have
+     * an event topic or do not have the proper topic subscription
+     * permissions.
+     *
+     * @param reference The reference to the service being added to the
+     *                  <code>ServiceTracker</code>.
+     * @return The service object to be tracked for the specified referenced
+     *         service or <code>null</code> if the specified referenced service
+     *         should not be tracked.
+     */
     @Override
     public Object addingService(ServiceReference reference)
     {
-        return super.addingService(reference);    //Todo change body of overridden methods use File | Settings | File Templates.
+        LOGGER.entering(CLASS_NAME, "addingService", reference);
+
+        Object test = reference.getProperty(EventConstants.EVENT_TOPIC);
+        if (test == null)
+        {
+            LOGGER.finest("Reference does not contain event topic, ignoring");
+            LOGGER.exiting(CLASS_NAME, "addingService", null);
+
+            return null;
+        }
+
+        Object service = context.getService(reference);
+
+        String[] topics;
+        if (test instanceof String)
+        {
+            topics = new String[]{(String)test};
+        }
+        else if (test instanceof String[])
+        {
+            topics = (String[])test;
+        }
+        else
+        {
+            LOGGER.finest("Reference contains event topic that is not a String or String[], ignoring");
+            LOGGER.exiting(CLASS_NAME, "addingService", null);
+
+            return null;
+        }
+
+        SecurityManager sm = System.getSecurityManager();
+        if (sm != null)
+        {
+            for (String topic : topics)
+            {
+                if (!service.getClass().getProtectionDomain().implies(new TopicPermission(topic, TopicPermission.SUBSCRIBE)))
+                {
+                    LOGGER.finest("Service does not have permission to subscribe for topic " + topic + ", ignoring");
+                    LOGGER.exiting(CLASS_NAME, "addingService", null);
+
+                    return null;
+                }
+            }
+        }
+
+        String[][] paths = new String[topics.length][];
+
+        for (int i = 0; i < topics.length; i++)
+        {
+            paths[i] = topics[i].split("/");
+
+            for (int j = 0; j < paths[i].length - 1; j++)
+            {
+                if ("*".equals(paths[i][j]))
+                {
+                    LOGGER.finest("Service has an ill formatted topic " + topics[i] + ", ignoring");
+                    LOGGER.exiting(CLASS_NAME, "addingService", null);
+
+                    return null;
+                }
+                paths[i][j] = paths[i][j].intern();
+            }
+            paths[i][paths[i].length - 1] = paths[i][paths[i].length - 1].intern();
+        }
+
+        String filter = (String)reference.getProperty(EventConstants.EVENT_FILTER);
+        try
+        {
+            EventListener listener = new EventListener(paths, reference, filter);
+
+            add(listener);
+
+            LOGGER.exiting(CLASS_NAME, "addingService", listener);
+
+            return listener;
+        }
+        catch (InvalidSyntaxException e)
+        {
+            LOGGER.finest("Service had an invalid filter " + filter + ", ignoring");
+            LOGGER.exiting(CLASS_NAME, "addingService", null);
+            return null;
+        }
     }
 
     @Override
     public void modifiedService(ServiceReference reference, Object service)
     {
-        super.modifiedService(reference, service);    //Todo change body of overridden methods use File | Settings | File Templates.
+        LOGGER.entering(CLASS_NAME, "modifiedService", new Object[]{reference, service});
+
+        removedService(reference, service);
+        addingService(reference);
+
+        LOGGER.exiting(CLASS_NAME, "modifiedService", null);
     }
 
     @Override
     public void removedService(ServiceReference reference, Object service)
     {
-        super.removedService(reference, service);    //Todo change body of overridden methods use File | Settings | File Templates.
+        LOGGER.entering(CLASS_NAME, "removedService", new Object[]{reference, service});
+
+        context.ungetService(reference);
+        remove((EventListener)service);
+
+        LOGGER.exiting(CLASS_NAME, "removedService", null);
     }
 
     public void postEvent(final Event event)
     {
-        LOGGER.entering(CLASS_NAME, "", event);
+        LOGGER.entering(CLASS_NAME, "postEvent", event);
+
+        SecurityManager sm = System.getSecurityManager();
+        if (sm != null) sm.checkPermission(new TopicPermission(event.getTopic(), TopicPermission.PUBLISH));
 
         Set<EventListener> set = collectListeners(event);
 
-        for (final EventListener eventListener : set)
+        for (final EventListener el : set)
         {
-            eventListener.executor.execute(new Runnable()
-            {
-                public void run()
-                {
-                    eventListener.handleEvent(event);
-                }
-            });
+            el.executor.execute(new TimeoutRunnable(el, event));
         }
 
         LOGGER.exiting(CLASS_NAME, "postEvent");
     }
 
-    public void sendEvent(Event event)
+    public void sendEvent(final Event event)
     {
         LOGGER.entering(CLASS_NAME, "sendEvent", event);
 
+        SecurityManager sm = System.getSecurityManager();
+        if (sm != null) sm.checkPermission(new TopicPermission(event.getTopic(), TopicPermission.PUBLISH));
+
         Set<EventListener> set = collectListeners(event);
 
-        for (EventListener eventListener : set)
+        final CountDownLatch latch = new CountDownLatch(set.size());
+        for (final EventListener listener : set)
         {
-            eventListener.handleEvent(event);
+            listener.executor.execute(new TimeoutRunnable(latch, listener, event));
+        }
+
+        try
+        {
+            latch.await();
+        }
+        catch (InterruptedException ie)
+        {
+            LOGGER.log(Level.WARNING, "Wait interrupted", ie);
+            Thread.currentThread().interrupt();
         }
 
         LOGGER.exiting(CLASS_NAME, "sendEvent");
     }
 
+    private void add(EventListener listener)
+    {
+        for (String[] tokens : listener.paths)
+        {
+            Listeners lPtr = listeners;
+
+            for (int i = 0; i < tokens.length - 1; i++)
+            {
+                synchronized (lPtr.listeners)
+                {
+                    Listeners check = lPtr.listeners.get(tokens[i]);
+                    if (check == null) lPtr.listeners.put(tokens[i], check = new Listeners());
+                    lPtr = check;
+                }
+            }
+
+            String token = tokens[tokens.length - 1];
+            if ("*".equals(token)) token = null;
+
+            synchronized (lPtr.handlers)
+            {
+                Set<EventListener> set = lPtr.handlers.get(token);
+                if (set == null) lPtr.handlers.put(token, set = new HashSet<EventListener>());
+                set.add(listener);
+            }
+        }
+    }
+
+    private void remove(EventListener listener)
+    {
+        for (String[] tokens : listener.paths)
+        {
+            Listeners lPtr = listeners;
+
+            for (int i = 0; i < tokens.length - 1; i++)
+            {
+                lPtr = lPtr.listeners.get(tokens[i]);
+                if (lPtr == null) return;
+            }
+
+            String token = tokens[tokens.length - 1];
+            if ("*".equals(token)) token = null;
+
+            Set<EventListener> set = lPtr.handlers.get(token);
+            if (set == null) return;
+            set.remove(listener);
+        }
+    }
+
     private Set<EventListener> collectListeners(Event event)
     {
-        Listeners l = listeners;
+        Listeners lPtr = listeners;
         Set<EventListener> set = new HashSet<EventListener>();
 
-        addListeners(set, l.getHandlers(), event);
-
-        for (String token : event.getTopic().split("/"))
+        String[] tokens = event.getTopic().split("/");
+        for (int i = 0; i < tokens.length - 1; i++)
         {
-            l = l.getListeners().get(token);
+            addListeners(set, lPtr.handlers.get(null), event);
 
-            if (l == null) break;
-
-            addListeners(set, l.getHandlers(), event);
+            lPtr = lPtr.listeners.get(tokens[i]);
+            if (lPtr == null) break;
         }
+
+        if (lPtr != null)
+        {
+            addListeners(set, lPtr.handlers.get(null), event);
+            addListeners(set, lPtr.handlers.get(tokens[tokens.length - 1]), event);
+        }
+
         return set;
     }
 
@@ -157,47 +335,79 @@ public class EventManagerImpl extends ServiceTracker implements EventAdmin
     {
         for (EventListener eventListener : from)
         {
-            if (event.matches(eventListener.getFilter())) to.add(eventListener);
+            if (event.matches(eventListener.filter)) to.add(eventListener);
+        }
+    }
+
+    private class TimeoutRunnable implements Runnable
+    {
+        private final CountDownLatch latch;
+        private final EventListener listener;
+        private final Event event;
+
+        TimeoutRunnable(EventListener listener, Event event)
+        {
+            this(null, listener, event);
+        }
+
+        TimeoutRunnable(CountDownLatch latch, EventListener listener, Event event)
+        {
+            this.latch = latch;
+            this.listener = listener;
+            this.event = event;
+        }
+
+        public void run()
+        {
+            ScheduledFuture future = null;
+            try
+            {
+                future = scheduledExecutor.schedule(new Runnable()
+                {
+                    public void run()
+                    {
+                        remove(listener);
+                    }
+                }, timeout, timeUnit);
+
+                listener.handleEvent(event);
+            }
+            catch (Throwable t)
+            {
+                LOGGER.log(Level.WARNING, "Listener threw exception", t);
+            }
+            finally
+            {
+                if (future != null) future.cancel(false);
+                if (latch != null) latch.countDown();
+            }
         }
     }
 
     private static class Listeners
     {
-        Map<String, Listeners> listeners = new Hashtable<String, Listeners>();
-        Set<EventListener> handlers = Collections.synchronizedSet(new HashSet<EventListener>());
-
-        public Map<String, Listeners> getListeners()
-        {
-            return listeners;
-        }
-
-        public Set<EventListener> getHandlers()
-        {
-            return handlers;
-        }
+        private final Map<String, Listeners> listeners = new Hashtable<String, Listeners>();
+        private final Map<String, Set<EventListener>> handlers = Collections.synchronizedMap(new HashMap<String, Set<EventListener>>());
     }
 
     private class EventListener implements EventHandler
     {
         private final Executor executor;
+        private final String[][] paths;
         private final ServiceReference reference;
         private final Filter filter;
 
-        private EventListener(Executor executor, ServiceReference reference, String filter) throws InvalidSyntaxException
+        private EventListener(String[][] paths, ServiceReference reference, String filter) throws InvalidSyntaxException
         {
-            this.executor = executor;
+            this.executor = new SerialExecutor(EventManagerImpl.this.executor);
+            this.paths = paths;
             this.reference = reference;
             this.filter = (filter == null ? DEFAULT_FILTER : context.createFilter(filter));
         }
 
-        public Filter getFilter()
-        {
-            return filter;
-        }
-
         public void handleEvent(Event event)
         {
-            EventHandler handler = (EventHandler) context.getService(reference);
+            EventHandler handler = (EventHandler)context.getService(reference);
             if (handler != null) handler.handleEvent(event);
         }
     }
